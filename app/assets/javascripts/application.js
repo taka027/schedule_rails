18192fa78d9a67c500cca4c8ba0458e6da0e39f2
taka027 <kanekotakashi2009@gmail.com>
2018-10-08 09:58:53 +0000

    datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require jquery
+//= require jquery_ujs
+//= require jquery-ui/widgets/datepicker
+//= require jquery-ui/i18n/datepicker-ja
 //= require_tree .
